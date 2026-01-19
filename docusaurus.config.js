be1e46b0524d1080fe3b3e7d0eedd62f1b7e8b2f
bgravalo <bruna.gravalos@bsc.es>
2026-01-19 12:20:23 +0100

    some changes for deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -134,10 +134,7 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
+              
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
