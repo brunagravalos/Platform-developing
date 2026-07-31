d0df9353b657eede186c3f6ff59512b557acc8c3
Rodrigo Sanches Pereira Guerra <247262904+rspguerra@users.noreply.github.com>
2026-07-31 15:43:40 +0200

    Deploy website - based on 7cc5c9109e5dff16c0d6c2cf7f0cba8709269b6d

---
+++ assets/js/34ac83bf.2b6782e6.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkmy_website=globalThis.webpackChunkmy_website||[]).push([[445],{2862(e){e.exports=JSON.parse('{"metadata":{"permalink":"/Platform-developing/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":0,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
